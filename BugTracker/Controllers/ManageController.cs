2561bac6c0ba22e99c041eef03882f9a889c2240
Ele <ecpalmer21@gmail.com>
2016-05-10 13:34:29 -0400

    user can add phone number

--- BugTracker/Controllers/ManageController.cs
+++ BugTracker/Controllers/ManageController.cs
@@ -117,22 +117,35 @@ namespace BugTracker.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> AddPhoneNumber(AddPhoneNumberViewModel model)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                return View(model);
-            }
-            // Generate the token and send it
-            var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId(), model.Number);
-            if (UserManager.SmsService != null)
-            {
-                var message = new IdentityMessage
+                var userId = User.Identity.GetUserId();
+                var user = await UserManager.FindByIdAsync(userId);
+                user.PhoneNumber = model.Number;
+                var result = await UserManager.UpdateAsync(user);
+
+                if (!result.Succeeded)
                 {
-                    Destination = model.Number,
-                    Body = "Your security code is: " + code
-                };
-                await UserManager.SmsService.SendAsync(message);
+                    AddErrors(result);
+                }
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("VerifyPhoneNumber", new { PhoneNumber = model.Number });
+            return RedirectToAction("AddPhoneNumber");
+
+            // Generate the token and send it
+            //var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId(), model.Number);
+            //if (UserManager.SmsService != null)
+            //{
+            //    var message = new IdentityMessage
+            //    {
+            //        Destination = model.Number,
+            //        Body = "Your security code is: " + code
+            //    };
+            //    await UserManager.SmsService.SendAsync(message);
+            //}
         }
 
         //
