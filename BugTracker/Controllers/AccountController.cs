d7214e276c387e633f92006e9c05504062eafd7d
Ele <ecpalmer21@gmail.com>
2016-05-19 14:24:55 -0400

    externalconfirmation asks for name

--- BugTracker/Controllers/AccountController.cs
+++ BugTracker/Controllers/AccountController.cs
@@ -369,7 +369,7 @@ namespace BugTracker.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
